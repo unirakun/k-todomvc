861f66c2a7e51d2c12b5d4b4283401582fe0b6ef
Guillaume CRESPEL <guillaume.crespel@gmail.com>
2018-03-23 17:23:49 +0100

    empty hoc

--- src/components/todo/todo.fetch.js
+++ src/components/todo/todo.fetch.js
@@ -1,22 +1,10 @@
 import React, { Component } from 'react'
 
-export default (url) => (WrappedComponent) => {
+export default (/* config */) => (WrappedComponent) => {
   return class extends Component {
-    static displayName = 'todoFetch'
-
-    componentWillMount = async () => {
-      const raw = await window.fetch(url, { method: 'GET', headers: { 'Accept': 'application/json' } })
-      const {message, subtitle} = await raw.json()
-      const label = `${message}${subtitle}`
-      
-      this.setState((state) => ({
-        ...state,
-        label,
-      }))
-    }
 
     render() {
-      return <WrappedComponent {...this.props} {...this.state} />
+      return <WrappedComponent {...this.props} />
     }
   }
 }
