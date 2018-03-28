f0288d9ddc64d734998dec85feb72dc606d4dc07
Guillaume CRESPEL <guillaume.crespel@gmail.com>
2018-03-28 13:29:38 +0200

    :sparkles: hoc fetch

--- src/components/todos/todos.fetch.js
+++ src/components/todos/todos.fetch.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react'
 
-export default () => (WrappedComponent) => {
+export default (url) => (WrappedComponent) => {
   return class extends Component {
-    
+    static displayName = 'todosFetch'
+
+    componentWillMount = async () => {
+      const raw = await window.fetch(url)
+      const data = await raw.json()
+      
+      this.setState((state) => ({
+        ...state,
+        data,
+      }))
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} {...this.state} />
+    }
   }
 }
