b3ac40da352b341d7b283d453a6848a8e6d3a9a6
Guillaume CRESPEL <guillaume.crespel@gmail.com>
2018-03-29 16:10:36 +0200

    :ok_hand:

--- src/components/newTodo/newTodo.container.js
+++ src/components/newTodo/newTodo.container.js
@@ -3,12 +3,16 @@ import newTodo from '../../store/reducers/newTodo'
 import todos from '../../store/reducers/todos'
 import Component from './newTodo'
 
+const makeTodo = label => ({ id: new Date().getTime(), label })
+
 const mapStateToProps = (state) => ({
   newTodo: newTodo.get()(state),
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  onKeyDown: e => e.which === 13 && dispatch(todos.add(e.target.value)) && dispatch(newTodo.reset()),
+  onKeyDown: e => e.which === 13 
+    && dispatch(todos.add(makeTodo(e.target.value))) 
+    && dispatch(newTodo.reset()),
   onChange: e => dispatch(newTodo.set(e.target.value)),
 })
 
--- src/store/reducers/todos.js
+++ src/store/reducers/todos.js
@@ -1,6 +1,3 @@
 import { keyValue } from 'k-redux-factory'
-import { mapPayload } from 'k-redux-factory/helpers'
 
-/* Todos Reducer  with her mapper */
-export const todosMapper = todo => ({ id: new Date().getTime(), label: todo.trim(), completed: false })
-export default keyValue({ pre: [mapPayload(/@@krf\/ADD>TODOS/)(todosMapper)] })({ path: 'data', key: 'id', name: 'todos' })
+export default keyValue({ path: 'data', key: 'id', name: 'todos' })
