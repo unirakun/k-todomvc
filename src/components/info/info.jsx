d0356bf7f4a5548547def54f393c2e2da2975a49
Guillaume CRESPEL <guillaume.crespel@gmail.com>
2020-02-05 15:36:37 +0100

    :memo: rebranding

--- src/components/info/info.jsx
+++ src/components/info/info.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default () => (
   <div className="info">
     <p>Double-click to edit a todo</p>
-    <p>Created by <a href="https://github.com/alakarteio"><b>alakarte</b></a></p>
+    <p>Created by <a href="https://github.com/unirakun"><b>uni rakun</b></a></p>
     <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
   </div>
 )
